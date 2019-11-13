32110d3de7de08543ab2800d28ba669ffb85d1a4
akueri0218 <akueri0218@gmail.com>
2019-11-13 17:31:44 +0900

    ver.0.1

--- GradationStudio/MainWindow.xaml.cs
+++ GradationStudio/MainWindow.xaml.cs
@@ -13,90 +13,185 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+
 using GS_BMP;
 using GS_Files;
 using GS;
 
 namespace GradationStudio
 {
+    class ColorIndex
+    {
+        public Pallet Pallet { get; set; }
+        public DockPanel Panel { get; set; }
+
+        public void SetPanel(Action<object, RoutedEventArgs> func)
+        {
+            Panel = new DockPanel();
+            Panel.Height = 30;
+
+            Border border = new Border();
+            border.Width = 30;
+            border.BorderThickness = new Thickness(2);
+            border.BorderBrush = Brushes.Black;
+            border.Background = new SolidColorBrush(Color.FromRgb(Pallet.Color.R, Pallet.Color.G, Pallet.Color.B));
+
+            Label colorLabel = new Label();
+            colorLabel.Width = 120;
+            colorLabel.Content = Pallet.Color.ToString();
+
+            Label offsetLabel = new Label();
+            offsetLabel.Width = 70;
+            offsetLabel.Content = Pallet.Pos.ToString();
+
+            Button button = new Button();
+            button.Width = 30;
+            button.Height = 30;
+            button.Content = "X";
+            button.Click += new RoutedEventHandler(func);
+
+            Panel.Children.Add(border);
+            Panel.Children.Add(colorLabel);
+            Panel.Children.Add(offsetLabel);
+            Panel.Children.Add(button);
+        }
+
+        public ColorIndex(Pallet pallet, Action<object, RoutedEventArgs> func)
+        {
+            Pallet = pallet;
+            SetPanel(func);
+        }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private BMP bmp = new BMP(@"C:\Windows\Fujitsu\wallpaper\2560-1440\FJPC003.png");
-        private ColorMap map;
+        List<ColorIndex> colorList = new List<ColorIndex>();
+        Pallet pallet = new Pallet(new GSColor(255, 255, 255), 0);
 
-        //private Gradation gradation;
+        WriteableBitmap SourceImage;
+        OpenFileDialog fileDialog;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            GradationDesigner designer = new GradationDesigner();
-            designer.Show();
         }
 
-        private void GSInit()
+        private void colorDelete(object sender, RoutedEventArgs e)
         {
-            byte[] rg = new byte[256 * 256];
-            byte[] gb = new byte[256 * 256];
-            byte[] br = new byte[256 * 256];
+            Button button = (Button)sender;
 
-            for(int i = 0; i < 256 * 256; i++)
+            DockPanel parent = (DockPanel)button.Parent;
+            Border border = parent.Children.OfType<Border>().First();
+            byte offset = 0;
+            foreach (Label label in parent.Children.OfType<Label>())
+                byte.TryParse((string)label.Content, out offset);
+
+            GSColor color = new GSColor(((SolidColorBrush)border.Background).Color.R, ((SolidColorBrush)border.Background).Color.G, ((SolidColorBrush)border.Background).Color.B);
+
+            colorList.RemoveAll(item => item.Pallet.Color.Equals(color) && item.Pallet.Pos == offset);
+
+            colorIndex.Children.Clear();
+            colorList.ForEach(item =>
             {
-                rg[i] = 255;
-                gb[i] = 255;
-                br[i] = 255;
-            }
-
-
-            map = new ColorMap(bmp.Pixels);
-
-            List<GSColor> colorList = new List<GSColor>(map.ColorList);
-
-            colorList.ForEach(color =>
-            {
-                rg[color.R + color.G * 256] -= 64;
-                gb[color.G + color.B * 256] -= 64;
-                br[color.B + color.R * 256] -= 64;
+                colorIndex.Children.Add(item.Panel);
             });
 
-            BitmapSource rg_img = BitmapSource.Create(256, 256, 350, 350, PixelFormats.Gray8, null, rg, 256);
-            BitmapSource gb_img = BitmapSource.Create(256, 256, 350, 350, PixelFormats.Gray8, null, gb, 256);
-            BitmapSource br_img = BitmapSource.Create(256, 256, 350, 350, PixelFormats.Gray8, null, br, 256);
+            UpdateGradation();
+        }
+        private void AddColor_Click(object sender, RoutedEventArgs e)
+        {
+            ColorIndex color = new ColorIndex(pallet, colorDelete);
 
-            RG.Source = rg_img;
-            GB.Source = gb_img;
-            BR.Source = br_img;
+            colorList.Add(color);
+            colorIndex.Children.Add(color.Panel);
 
-            ////source_map = new ColorMap(source_bmp.Pixels);
+            pallet = new Pallet(new GSColor(255, 255, 255), 0);
 
-            //gradation = new Gradation(map.ColorList);
-            ////source_gradation = new Gradation(source_map.ChunkColorList);
-
-            //List<GSColor> colorList = Gradation.MakeGradation(gradation.KeyColorPallet);
-
-            //colorList.ForEach(color =>
-            //{
-            //    Label label = new Label();
-            //    //label.Content = ((double)map.ChunkList[map.ChunkColorList.IndexOf(color)].PixelList.Count / (bmp.Width * bmp.Height) * 100).ToString("F5") + "%";
-            //    label.Background = color.AsSolidColor();
-            //    //if ((color.R + color.G + color.B) / 3 < 64) label.Foreground = Brushes.White;
-            //    label.HorizontalContentAlignment = HorizontalAlignment.Right;
-            //    label.Padding = new Thickness(0, 20, 10, 0);
-            //    label.Margin = new Thickness(0, 0, 0, 0);
-            //    label.Height = 3;
-            //    PaletteGrid.Children.Add(label);
-            //});
+            UpdateGradation();
         }
 
-        private void InitButton_Click(object sender, RoutedEventArgs e)
+        private void colorChanged(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            btn.IsEnabled = false;
 
-            GSInit();
+        }
+
+        private void offsetChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (textBox.Text == "")
+                return;
+
+            int output;
+
+            if (!int.TryParse(textBox.Text, out output))
+                return;
+
+            if (output >= 256)
+                return;
+
+            pallet.Pos = byte.Parse(textBox.Text);
+        }
+
+        private void UpdateGradation()
+        {
+            colorList.OrderBy(item => item.Pallet.Pos);
+
+
+            LinearGradientBrush gradient = new LinearGradientBrush();
+            gradient.StartPoint = new Point(0, 0);
+            gradient.EndPoint = new Point(0, 1);
+
+            colorList.ForEach(item =>
+            {
+                GradientStop stop = new GradientStop();
+                stop.Color = Color.FromRgb(item.Pallet.Color.R, item.Pallet.Color.G, item.Pallet.Color.B);
+                stop.Offset = (double)item.Pallet.Pos / 255;
+
+                gradient.GradientStops.Add(stop);
+            });
+
+
+            grdBar.Fill = gradient;
+        }
+
+        private void GRDLoad_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = FileDialog("gradation files (*.grd)|*.grd");
+        }
+
+        private void GRDSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GRDApply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImgOpen_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = FileDialog("image files (*.png;*.jpg;*.bmp)|*.png;*.jpg;*.bmp");
+
+            SourceImage = new WriteableBitmap(new BitmapImage(new Uri(@filename)));
+
+            image.Source = SourceImage;
+        }
+
+        private string FileDialog(string filter)
+        {
+            fileDialog = new OpenFileDialog();
+
+            fileDialog.Filter = filter;
+
+            if (fileDialog.ShowDialog() == false)
+                return null;
+
+            return fileDialog.FileName;
         }
     }
 }
